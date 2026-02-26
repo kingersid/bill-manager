919f6de0906c5bd4b14034d37aa888c1dfb992cc
siddharth <kinger.siddharth@gmail.com>
2026-02-26 18:45:54 +0000

    resolved changess smore

--- server/index.js
+++ server/index.js
@@ -197,7 +197,7 @@ app.get('/api/sales-trend', authenticateToken, async (req, res) => {
 });
 
 // Serve frontend for all other routes
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
